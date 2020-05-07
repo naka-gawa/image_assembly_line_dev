981ad1d8891829c86cfeb838fa0fb8513f43993e
naka-gawa <tomoaki-nakagawa@c-fo.com>
2020-05-07 19:08:32 +0900

    modify bug fix

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('1000')
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.setOutput('Hello World!!')
   } catch (error) {
     core.setFailed(error.message)
   }
